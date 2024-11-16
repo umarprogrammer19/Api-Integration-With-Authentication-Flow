2305f3462ed85ae4e531331e09471dc19cb69338
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-11-16 17:50:27 +0500

    User Models And Utils

---
+++ src/lib/utils.ts
@@ -0,0 +1,20 @@
+import { clsx, type ClassValue } from "clsx";
+import mongoose from "mongoose";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
+
+export const connectToDatabase = async () => {
+    try {
+        if (mongoose.connections && mongoose.connections[0].readyState) return;
+        const { connection } = await mongoose.connect(process.env.MONGODB_URI as string, {
+            dbName: "nextAuth",
+        });
+        console.log(`Connected To Database ${connection.host}`);
+
+    } catch (err) {
+        console.error("Error connecting to database");
+    }
+}
---
+++ src/models/userModels.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: "true" },
+    email: { type: String, required: "true" },
+    password: { type: String, select: "false" },
+});
+
+export const User = mongoose.models?.User || mongoose.model("User", userSchema);
