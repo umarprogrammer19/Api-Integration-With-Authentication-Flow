f5b668f4b0515951bafafe328d5a4a546f43aa87
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-11-16 17:57:28 +0500

    Form.tsx

---
+++ src/components/client/Form.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { toast } from "sonner";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { loginHandler } from "@/actions/login";
+import { useRouter } from "next/navigation";
+
+export default function LoginForm() {
+    const router = useRouter();
+    return (
+        <>
+            <form
+                action={async (formData): Promise<any> => {
+                    const email = formData.get("email") as string;
+                    const password = formData.get("password") as string;
+                    if (!email || !password) return toast.error("Email and password are required");
+                    const toastId = toast.loading("Logging in...");
+                    try {
+                        const error = await loginHandler(email, password);
+
+                        if (!error) {
+                            toast.success("Login successful", { id: toastId });
+                            router.refresh();
+                        } else if (error) {
+                            toast.error("Invalid Credentials", { id: toastId });
+                        }
+                    } catch (err) {
+                        console.error("Login error:", err);
+                        toast.error("Something went wrong. Please try again.", { id: toastId });
+                    }
+                }}
+                className="flex flex-col gap-4"
+            >
+                <Input type="email" placeholder="Email" name="email" />
+                <Input type="password" placeholder="Password" name="password" />
+                <Button type="submit">Login</Button>
+            </form>
+        </>
+    );
+}
