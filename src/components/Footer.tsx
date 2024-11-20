70efbfea3db860f1d650174f28e9e607c0e96609
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-11-20 19:13:03 +0500

    Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="bg-blue-900 text-white py-6 mt-12">
+            <div className="max-w-screen-xl mx-auto text-center">
+                <p>&copy; 2024 Your Name. All Rights Reserved.</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
