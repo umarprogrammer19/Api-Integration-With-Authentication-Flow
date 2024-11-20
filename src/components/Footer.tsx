b4a6f097020263722def8474e37683a394db6040
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-11-20 19:13:34 +0500

    Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="bg-blue-900 text-white py-6 mt-12">
             <div className="max-w-screen-xl mx-auto text-center">
-                <p>&copy; 2024 Your Name. All Rights Reserved.</p>
+                <p>&copy; 2024 Umar Farooq. All Rights Reserved.</p>
             </div>
         </footer>
     )
