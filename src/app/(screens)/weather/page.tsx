4fc073ab74027b75bfd37180c80b67ae65546319
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-11-20 22:31:56 +0500

    Improve

--- src/app/(screens)/weather/page.tsx
+++ src/app/(screens)/weather/page.tsx
@@ -1,6 +1,10 @@
+import { auth } from "@/auth";
 import WeatherWidget from "@/components/weather-widget";
+import { redirect } from "next/navigation";
 
-export default function Weather() {
+export default async function Weather() {
+    const session = await auth();
+    if (!session?.user) redirect("/login");
     return (
         <>
             <WeatherWidget />
